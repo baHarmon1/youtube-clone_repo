6a484e8f70d62e9caa72040b6217937562a123e4
Brycen Harmon <brycenharmon@gmail.com>
2021-11-23 11:28:00 -0600

    crated initial files for creation of react app

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (props) => {
+    return ( 
+        <div>
+            <h1>Test from App</h1>
+        </div>
+     );
+}
+ 
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import reactDom from 'react-dom';
+import App from './components/App'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
+
+reactDom.render(<App/>, document.getElementById('root'));
